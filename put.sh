c3fe757464da940115dd36f468add0c927ad2bfd
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-26 14:09:44 +0530

    added helper shell files into git ignore

--- put.sh
+++ put.sh
@@ -1,9 +1,9 @@
-curl -X PUT http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/courses/a97e22d2-bd33-4d55-a6ff-dd0e13468936  -H "Content-type: application/json" -d '{
-    "title": "todo list",
-    "content": "some important things",
-    "creator_name": "showdown",
-    "attachmentURL": "url/path/to/some/photo/attachment",
-    "category": "asdf", 
-    "keyword": "bot",
-    "contact": "Jason mamoa.com"
-}'
+    curl -X PUT http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/courses/154ececb-4a82-4abe-9d79-9e3cda5aba9e  -H "Content-type: application/json" -d '{
+        "title": "todo list",
+        "content": "some important things",
+        "creatorName": "showdown",
+        "attachmentURL": "path",
+        "category": "asdf", 
+        "keyword": "bot",
+        "contact": "Jason mamoa.com"
+    }'
