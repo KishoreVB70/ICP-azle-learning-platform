9d001faecbaeaa78ade947e92e55aa5666d989ba
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-27 11:31:18 +0530

    added helper shell scripts

---
+++ helper shell files/post.sh
@@ -0,0 +1,9 @@
+curl -X POST http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/courses  -H "Content-type: application/json" -d '{
+    "title": "How to create a Typescript azle project",
+    "content": "abc",
+    "creatorName": "kishore",
+    "attachmentURL": "url/",
+    "category": "programming", 
+    "keyword": "azle",
+    "contact": "github.com/kishorevb70"
+}'
---
+++ helper shell files/put.sh
@@ -0,0 +1,9 @@
+curl -X PUT http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/courses/a97e22d2-bd33-4d55-a6ff-dd0e13468936  -H "Content-type: application/json" -d '{
+    "title": "How to create a Typescript azle project",
+    "content": "abc",
+    "creatorName": "kishore",
+    "attachmentURL": "url/",
+    "category": "programming", 
+    "keyword": "Azle",
+    "contact": "github.com/kishorevb70"
+}'
